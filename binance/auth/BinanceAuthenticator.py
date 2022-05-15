3e1e1b5cd71310d2b6ffddcfeb85b3f369e05f57
Eugene the Red <me@eugenethered.com>
2022-05-15 14:19:30 +0100

    implemented authentication + termination

--- binance/auth/BinanceAuthenticator.py
+++ binance/auth/BinanceAuthenticator.py
@@ -9,6 +9,7 @@ class BinanceAuthenticator(Authenticator):
 
     def __init__(self, options):
         super().__init__(options)
+        self.auth_headers = self.build_auth_header()
         self.listen_key = None
 
     def should_update_url(self) -> bool:
@@ -17,14 +18,24 @@ class BinanceAuthenticator(Authenticator):
     def update_url(self, url) -> str:
         return url.format(self.listen_key)
 
-    def authenticate(self):
+    async def authenticate(self):
         logging.info(f'authenticating... via auth url:{self.auth_url}')
-        api_key = self.obtain_api_info()
-        headers = {'X-MBX-APIKEY': api_key}
-        response = requests.post(self.auth_url, headers=headers)
+        response = requests.post(self.auth_url, headers=self.auth_headers)
         data = response.json()
         self.listen_key = as_data(data, 'listenKey')
-        logging.info(f'authentication complete via url:{self.auth_url}')
+        logging.info('authentication complete')
+
+    async def terminate(self):
+        logging.info(f'terminating authentication... via auth url:{self.auth_url}')
+        parameters = {'listenKey': self.listen_key}
+        response = requests.delete(self.auth_url, headers=self.auth_headers, params=parameters)
+        status = response.status_code
+        logging.info(f'authentication terminated {status}')
+
+    def build_auth_header(self):
+        api_key = self.obtain_api_info()
+        header = {'X-MBX-APIKEY': api_key}
+        return header
 
     def obtain_api_info(self):
         return self.obtain_auth_value('API_KEY')
