b3d6bf761fa9e4cae504473dcec012a09c331736
Eugene the Red <me@eugenethered.com>
2022-07-29 18:29:55 +0100

    bump version + deps

--- simulations/authconfig/__init__.py
+++ simulations/authconfig/__init__.py
@@ -4,7 +4,7 @@ from cache.holder.RedisCacheHolder import RedisCacheHolder
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
         'AUTH_INFO_KEY': 'binance:auth:info'
     }
--- simulations/authlistenkey/__init__.py
+++ simulations/authlistenkey/__init__.py
@@ -7,7 +7,7 @@ from binance.auth.BinanceAuthenticator import BinanceAuthenticator
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
         'AUTH_INFO_KEY': 'binance:auth:info',
         'AUTH_URL': 'https://api.binance.com/api/v3/userDataStream'
